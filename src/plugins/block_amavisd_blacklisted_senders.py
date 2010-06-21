7f2b812f58ceefbcff55bdeea58b982843f26c5e
Zhang Huangbin <Zhang Huangbin@localhost>
2010-06-21 23:24:29 +0800

    Fix incorrect sender list in plugin: block_amavisd_blacklisted_senders.py.

--- src/plugins/block_amavisd_blacklisted_senders.py
+++ src/plugins/block_amavisd_blacklisted_senders.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# Author: Zhang Huangbin <michaelbibby (at) gmail.com>
+# Author: Zhang Huangbin (zhb@iredmail.org)
 
 import sys
 
 def restriction(smtpSessionData, ldapRecipientLdif, **kargs):
     # Get sender address.
     sender = smtpSessionData.get('sender').lower()
+    splited_sender_domain = str(sender.split('@')[-1]).split('.')
+
+    # Get correct domain name and sub-domain name.
+    # Sample sender domain: sub2.sub1.com.cn
+    #   -> sub2.sub1.com.cn
+    #   -> .sub2.sub1.com.cn
+    #   -> .sub1.com.cn
+    #   -> .com.cn
+    #   -> .cn
+    list_senders = [sender, '@'+sender.split('@')[-1],]
+    for counter in range(len(splited_sender_domain)):
+        # Append domain and sub-domain.
+        list_senders += ['@.' + '.'.join(splited_sender_domain)]
+        splited_sender_domain.pop(0)
 
     # Get value of amavisBlacklistedSender.
-    blSenders = ldapRecipientLdif.get('amavisBlacklistSender', [])
+    blSenders = [v.lower() for v in ldapRecipientLdif.get('amavisBlacklistSender', [])]
 
-    if sender in [ v.lower() for v in blSenders ]:
+    if len(set(list_senders) & set(blSenders)) > 0:
+        # Reject blacklisted senders.
         return 'REJECT Not Authorized'
     else:
         return 'DUNNO'
