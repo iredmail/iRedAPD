d5294c8d0dcab354f468f6fe03bd5821d20074f7
Zhang Huangbin <zhb@iredmail.org>
2013-03-07 14:08:31 +0800

    Move custom settings to settings.py instead of plugin source file.

--- plugins/reject_sender_login_mismatch.py
+++ plugins/reject_sender_login_mismatch.py
@@ -1,11 +1,15 @@
 """Reject sender login mismatch (sender in mail header and SASL username).
 
-You should remove "sender_login_mismatch" in Postfix
-"smtpd_sender_restrictions" and let this plugin do it for you.
+*) You should remove "sender_login_mismatch" in Postfix
+   "smtpd_sender_restrictions" and let this plugin do it for you.
+
+*) Please list all allowed senders in in settings.py, parameter
+   ALLOWED_LOGIN_MISMATCH_SENDERS, either a list or tuple is ok.
 """
 
 import logging
 from libs import SMTP_ACTIONS
+import settings
 
 REQUIRE_LOCAL_SENDER = False
 REQUIRE_LOCAL_RECIPIENT = False
@@ -13,7 +17,10 @@ SENDER_SEARCH_ATTRLIST = []
 RECIPIENT_SEARCH_ATTRLIST = []
 
 # Allow sender login mismatch for below senders.
-ALLOWED_SENDERS = []
+if 'ALLOWED_LOGIN_MISMATCH_SENDERS' in dir(settings):
+    ALLOWED_LOGIN_MISMATCH_SENDERS = settings.ALLOWED_LOGIN_MISMATCH_SENDERS
+else:
+    ALLOWED_LOGIN_MISMATCH_SENDERS = []
 
 def restriction(**kwargs):
     # The sender appears in 'From:' header.
@@ -27,7 +34,7 @@ def restriction(**kwargs):
     if sasl_username:    # Is a outgoing email
         # Compare them
         if sender != sasl_username:
-            if sasl_username in ALLOWED_SENDERS:
+            if sasl_username in ALLOWED_LOGIN_MISMATCH_SENDERS:
                 return SMTP_ACTIONS['default']
             else:
                 # Reject without reason.
