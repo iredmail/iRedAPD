53c9f51fdd0bbce66f69bee9aaaa4728db1055d1
Zhang Huangbin <zhb@iredmail.org>
2015-12-23 00:14:39 +0800

    Add greylisting whitelists for Amazon (amazon.com, amazonses.com).

--- SQL/greylisting_whitelists.sql
+++ SQL/greylisting_whitelists.sql
@@ -58,6 +58,30 @@ INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '262
 INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '216.136.162.65', 'LinkedIn');
 INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '199.101.161.130', 'LinkedIn');
 
+-- Amazone SES.
+-- Get by query DNS: dig TXT amazonses.com +short| grep 'v=spf1'
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '199.255.192.0/22', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '199.127.232.0/22', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '54.240.0.0/18', 'Amazon');
+-- Amazon
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '52.95.49.88/29', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '52.95.48.152/29', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '72.21.217.142/32', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '205.251.233.36/32', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '205.251.233.32/32', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '106.50.16.0/28', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '176.32.127.0/24', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '176.32.105.0/24', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '178.236.10.128/26', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '72.21.212.0/25', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '203.81.17.0/24', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '212.123.28.40/32', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '194.7.41.152/28', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '194.154.193.192/27', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '72.21.192.0/19', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '87.238.80.0/21', 'Amazon');
+INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '207.171.160.0/19', 'Amazon');
+
 -- INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '168.100.1.3', 'postfix.org');
 -- INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '168.100.1.4', 'postfix.org');
 -- INSERT INTO greylisting_whitelists (account, sender, comment) VALUES ('@.', '168.100.1.7', 'postfix.org');
