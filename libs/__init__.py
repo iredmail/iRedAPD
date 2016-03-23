be073cdbaff2b10750e46d1958c127ec5e36c122
Zhang Huangbin <zhb@iredmail.org>
2016-03-23 11:02:05 +0800

    Swap orders of plugin priority: reject_sender_login_mismatch -> 90, greylisting -> 80.

--- libs/__init__.py
+++ libs/__init__.py
@@ -32,8 +32,8 @@ SMTP_ACTIONS = {
 PLUGIN_PRIORITIES = {
     'reject_null_sender': 100,
     'reject_to_hostname': 99,
-    'greylisting': 90,
-    'reject_sender_login_mismatch': 80,
+    'reject_sender_login_mismatch': 90,
+    'greylisting': 80,
     'ldap_force_change_password_in_days': 70,
     'sql_force_change_password_in_days': 71,
     'throttle': 60,
