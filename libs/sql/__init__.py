85d6cf6f0740198e67a467de0b3d23c703a059b7
Zhang Huangbin <zhb@iredmail.org>
2016-07-22 19:09:46 +0800

    Fixed: function is_local_domain() doesn't query real mail domain for SQL backends.

--- libs/sql/__init__.py
+++ libs/sql/__init__.py
@@ -1,3 +1,5 @@
+from web import sqlquote
+
 from libs.logger import logger
 from libs import utils
 
@@ -9,11 +11,25 @@ def is_local_domain(conn, domain):
         return True
 
     try:
+        sql_quote_domain = sqlquote(domain)
+        sql = """SELECT domain
+                   FROM domain
+                  WHERE domain=%s
+                  LIMIT 1""" % sql_quote_domain
+        logger.debug('[SQL] query local domain (%s): \n%s' % (domain, sql))
+
+        qr = conn.execute(sql)
+        sql_record = qr.fetchone()
+        logger.debug('SQL query result: %s' % str(sql_record))
+
+        if sql_record:
+            return True
+
         sql = """SELECT alias_domain
                    FROM alias_domain
-                  WHERE alias_domain='%s' OR target_domain='%s'
-                  LIMIT 1""" % (domain, domain)
-        logger.debug('[SQL] query alias domains: \n%s' % sql)
+                  WHERE alias_domain=%s OR target_domain=%s
+                  LIMIT 1""" % (sql_quote_domain, sql_quote_domain)
+        logger.debug('[SQL] query alias domains (%s): \n%s' % (domain, sql))
 
         qr = conn.execute(sql)
         sql_record = qr.fetchone()
