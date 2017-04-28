e8db39f409e4ac2fc64e151fd14109fedc798992
Zhang Huangbin <zhb@iredmail.org>
2017-04-28 17:57:08 +0800

    Don't block other rdns names.

--- SQL/blacklist_rdns.sql
+++ SQL/blacklist_rdns.sql
@@ -1,7 +1,2 @@
-INSERT INTO blacklist_rdns (rdns) VALUES ('.bestel.com.mx');
-INSERT INTO blacklist_rdns (rdns) VALUES ('.hfc.comcastbusiness.net');
-INSERT INTO blacklist_rdns (rdns) VALUES ('.in-addr.btopenworld.com');
-INSERT INTO blacklist_rdns (rdns) VALUES ('.next-gen.ro');
-INSERT INTO blacklist_rdns (rdns) VALUES ('.static.cloud-ips.com');
 -- Chinese
 INSERT INTO blacklist_rdns (rdns) VALUES ('.dynamic.163data.com.cn');
