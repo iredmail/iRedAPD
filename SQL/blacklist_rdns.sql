5e3cbced50e508ffa7830653b60a29e61a907f20
Zhang Huangbin <zhb@iredmail.org>
2017-04-28 17:47:19 +0800

    Don't blacklist rdns name '.hinet-ip.hinet.net'.

--- SQL/blacklist_rdns.sql
+++ SQL/blacklist_rdns.sql
@@ -1,10 +1,7 @@
--- Chinese
-INSERT INTO blacklist_rdns (rdns) VALUES ('.dynamic.163data.com.cn');
--- Taiwan
-INSERT INTO blacklist_rdns (rdns) VALUES ('.hinet-ip.hinet.net');
--- Misc
+INSERT INTO blacklist_rdns (rdns) VALUES ('.bestel.com.mx');
+INSERT INTO blacklist_rdns (rdns) VALUES ('.hfc.comcastbusiness.net');
 INSERT INTO blacklist_rdns (rdns) VALUES ('.in-addr.btopenworld.com');
 INSERT INTO blacklist_rdns (rdns) VALUES ('.next-gen.ro');
-INSERT INTO blacklist_rdns (rdns) VALUES ('.hfc.comcastbusiness.net');
 INSERT INTO blacklist_rdns (rdns) VALUES ('.static.cloud-ips.com');
-INSERT INTO blacklist_rdns (rdns) VALUES ('.bestel.com.mx');
+-- Chinese
+INSERT INTO blacklist_rdns (rdns) VALUES ('.dynamic.163data.com.cn');
