8c0157437dc53febf4668f3033b4b1f0e224766a
Zhang Huangbin <zhb@iredmail.org>
2019-10-02 17:40:52 +0800

    Mention senderscore website in reject message.

--- libs/__init__.py
+++ libs/__init__.py
@@ -21,7 +21,7 @@ SMTP_ACTIONS = {
     # Throttling
     'reject_quota_exceeded': 'REJECT Quota exceeded',
     # Sender Score
-    'reject_low_sender_score': 'REJECT Server IP address has bad reputation',
+    'reject_low_sender_score': 'REJECT Server IP address has bad reputation. FYI: https://www.senderscore.org/',
     'greylisting': '451 4.7.1',
 }
 
