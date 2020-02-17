9e8d8b8d61caf3553ecea0eca1295eaa9c685835
Zhang Huangbin <zhb@iredmail.org>
2020-02-17 13:49:47 +0800

    Sync from master.

--- libs/utils.py
+++ libs/utils.py
@@ -6,10 +6,12 @@ import time
 import socket
 import subprocess
 import smtplib
+import uuid
 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
+from email.utils import formatdate
 
 from sqlalchemy import create_engine
 from web import sqlquote
@@ -636,6 +638,9 @@ def sendmail(subject, mail_body, from_address=None, recipients=None):
 
     msg['To'] = ','.join(recipients)
     msg['Subject'] = Header(subject, 'utf-8')
+    msg['Date'] = formatdate(usegmt=True)
+    msg['Message-Id'] = '<' + str(uuid.uuid4()) + '>'
+
     msg_body_plain = MIMEText(mail_body, 'plain', 'utf-8')
     msg.attach(msg_body_plain)
 
