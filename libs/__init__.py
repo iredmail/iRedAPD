4e08b53aebf076f0ea473ea3056880fa4370f909
Zhang Huangbin <zhb@iredmail.org>
2020-03-19 22:28:27 +0800

    Code cleanup.

--- libs/__init__.py
+++ libs/__init__.py
@@ -19,7 +19,7 @@ SMTP_ACTIONS = {
     'reject_message_size_exceeded': 'REJECT Message size exceed (maybe caused by big attachment file)',
     'reject_blacklisted_rdns': 'REJECT Blacklisted reverse DNS name of server IP address',
     # Throttling
-    'reject_quota_exceeded': 'REJECT Quota exceeded',
+    'reject_quota_exceeded': 'REJECT Throttling quota exceeded',
     # Sender Score
     'reject_low_sender_score': 'REJECT Server IP address has bad reputation. FYI: https://www.senderscore.org/lookup.php?lookup=',
     'greylisting': '451 4.7.1',
