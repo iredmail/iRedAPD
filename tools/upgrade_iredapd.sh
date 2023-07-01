114f99384369e28933b64216584a7b9ba6f951e9
Zhang Huangbin <zhb@iredmail.org>
2023-07-01 14:26:00 +0800

    Mark Debian 12 as supported release.

--- tools/upgrade_iredapd.sh
+++ tools/upgrade_iredapd.sh
@@ -105,7 +105,9 @@ if [ X"${KERNEL_NAME}" == X'LINUX' ]; then
         export DISTRO='DEBIAN'
 
         # Set distro code name and unsupported releases.
-        if grep -i '^11' /etc/debian_version &>/dev/null; then
+        if grep -i '^12' /etc/debian_version &>/dev/null; then
+            export DISTRO_VERSION='12'
+        elif grep -i '^11' /etc/debian_version &>/dev/null; then
             export DISTRO_VERSION='11'
         elif grep -i '^10' /etc/debian_version &>/dev/null; then
             export DISTRO_VERSION='10'
