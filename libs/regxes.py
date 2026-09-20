7fb667eee5520f7ea20aa46c6a588426b52edd4b
Zhang Huangbin <2048991+iredmail@users.noreply.github.com>
2026-09-20 22:11:45 +0800

    Revert last commit.

--- libs/regxes.py
+++ libs/regxes.py
@@ -10,11 +10,11 @@ import re
 # Although many special characters are allowed in username part of email
 # address, but that doesn't mean it's a good idea/choice, please try to use
 # only letters `[a-z]` and `.-_` in username.
-regx_email = r'''[\w#'~][\w.+=/&#'~-]*[\w#'~]@(?!.*\.\.)(?!-)[a-zA-Z0-9]([a-zA-Z0-9.-]*[a-zA-Z0-9])?\.(?!-)[a-zA-Z]{2,}'''
+regx_email = r'''[\w\-#'~][\w\-\.\+\=\/\&#'~]*@[\w\-][\w\-\.]*\.[a-zA-Z0-9\-]{2,25}'''
 cmp_email = re.compile(regx_email, re.IGNORECASE | re.DOTALL)
 
 # Domain name
-regx_domain = r'''[\w\-][\w\-\.]*\.[a-z]{2,15}'''
+regx_domain = r'''[\w\-][\w\-\.]*\.[a-z]{2,25}'''
 cmp_domain = re.compile(regx_domain, re.IGNORECASE | re.DOTALL)
 
 regx_top_level_domain = r'''[a-z0-9\-]{2,25}'''
